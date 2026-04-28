7ef650a62b42fffcc91d93ffd1a3b12f4a0b8eac
Franco Novoa <franconovoa3@gmail.com>
2026-04-28 11:38:13 -0500

    Create Vite React app - initial commit

--- web/build_wasm.bat
+++ web/build_wasm.bat
@@ -6,12 +6,11 @@ set EMSDK_PATH=C:\Users\Birits\Emscripten\emsdk
 
 call "%EMSDK_PATH%\emsdk_env.bat"
 
-
 em++ -std=c++20 -O2 ^
-  CircuitSimulator/parser.cpp ^
-  CircuitSimulator/circuit.cpp ^
-  CircuitSimulator/sim_api.cpp ^
-  -I CircuitSimulator ^
+  ../CircuitSimulator/parser.cpp ^
+  ../CircuitSimulator/circuit.cpp ^
+  ../CircuitSimulator/sim_api.cpp ^
+  -I ../CircuitSimulator ^
   -o public/circuit_sim.js ^
   -sEXPORTED_FUNCTIONS="[\"_runDC\",\"_runAC\",\"_runTransient\"]" ^
   -sEXPORTED_RUNTIME_METHODS="[\"ccall\",\"cwrap\"]" ^
