23f81892ed022abb2eda7d84f11c119e3df7f2b0
Franco Novoa <franconovoa3@gmail.com>
2026-04-28 21:55:35 -0500

    Prep for github deployment

---
+++ web/build_wasm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+em++ -std=c++20 -O2 \
+  CircuitSimulator/parser.cpp \
+  CircuitSimulator/circuit.cpp \
+  CircuitSimulator/sim_api.cpp \
+  -I CircuitSimulator \
+  -o web/public/circuit_sim.js \
+  -sEXPORTED_FUNCTIONS='["_runDC","_runAC","_runTransient"]' \
+  -sEXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' \
+  -sALLOW_MEMORY_GROWTH=1 \
+  -sNO_EXIT_RUNTIME=1 \
+  -sENVIRONMENT=web
